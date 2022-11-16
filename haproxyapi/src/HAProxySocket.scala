c20c5fd868271643f29b367ef64eddeacf5111e7
Jeff Dyke <jeff.dyke@gmail.com>
2022-11-16 13:21:41 -0500

    add new socket reader

---
+++ haproxyapi/src/HAProxySocket.scala
@@ -0,0 +1,47 @@
+package haproxyapi
+
+import cats.effect.{IO, Resource}
+import java.io._
+import java.net.{InetAddress, Socket}
+import cats.effect.unsafe.implicits.global
+
+object HAProxySocket {
+  def socket(hostname: String, port: Int): Resource[IO, Socket] =
+    Resource.make {
+      IO.pure(new Socket(InetAddress.getByName(hostname), port))
+    } { connection =>
+
+      IO.blocking(connection.close()).handleErrorWith(e => IO.println(e))
+    }
+
+  def outputStream(socket: Socket): Resource[IO, PrintWriter] =
+    Resource.make {
+      IO.blocking(new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))))
+    } { outString =>
+      IO.blocking(outString.close()).handleErrorWith(_ => IO.unit)
+    }
+  def inputStream(socket: Socket): Resource[IO, InputStreamReader] =
+    Resource.make {
+      IO.blocking(new InputStreamReader(socket.getInputStream))
+    } { in =>
+      IO.blocking(in.close()).handleErrorWith(_ => IO.unit)
+    }
+
+  def socketRequest(cmd: String) =
+    (
+      for {
+        sock <- socket("localhost", 9999)
+        out <- outputStream(sock)
+        in <- inputStream(sock)
+      } yield (out, in)
+    ).use { case (outStream, inStream) =>
+      for {
+        _ <- IO.pure(outStream.println(cmd))
+        _ <- IO.pure(outStream.flush())
+        in <- IO.blocking(new BufferedReader(inStream))
+        output <- IO.blocking(in.lines().toList())
+      } yield output
+
+    }.unsafeRunSync()
+}
+//new java.io.ByteArrayInputStream(command.getBytes(java.nio.charset.StandardCharsets.UTF_8.name))
